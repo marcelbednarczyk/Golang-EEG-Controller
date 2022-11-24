4f1d103813e66cc24ae3461adc226271d6db8e68
Marcel <marcelbednarczyk@onet.pl>
2022-11-24 17:32:58 +0100

    Iot

--- cmd/controlMental.go
+++ cmd/controlMental.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/marcelbednarczyk/Golang-EEG-Controller/pkg/cortex"
 	"golang.org/x/net/websocket"
@@ -22,6 +25,8 @@ func controlMental(ws *websocket.Conn, cortexToken, sessionID string) error {
 		return err
 	}
 
+	client := http.Client{}
+
 	for i := 0; i < 100; i++ {
 		data, err := receive[cortex.DataSample](ws)
 		if err != nil {
@@ -30,7 +35,19 @@ func controlMental(ws *websocket.Conn, cortexToken, sessionID string) error {
 
 		for i := range data.Com {
 			if i+1 < len(data.Com) {
-				log.Printf("Action: %s\t Power: %f\n", data.Com[i], data.Com[i+1])
+				fmt.Printf("Action: %s\t Power: %f\n", data.Com[i], data.Com[i+1])
+				s := 0.0
+				if s, err = strconv.ParseFloat(fmt.Sprintf("%f", data.Com[i+1]), 32); err != nil {
+					fmt.Println("Error: ", err)
+					return err
+				}
+				if data.Com[i] == "lift" && s > 0.5 {
+					fmt.Println("Fireplace!")
+					httpGet(&client, os.Getenv("IOT_URL")+"/update", map[string]string{"state": "0"})
+				} else if data.Com[i] == "push" && s > 0.5 {
+					fmt.Println("Snow!")
+					httpGet(&client, os.Getenv("IOT_URL")+"/update", map[string]string{"state": "1"})
+				}
 			}
 		}
 	}
@@ -46,7 +63,7 @@ func controlMental(ws *websocket.Conn, cortexToken, sessionID string) error {
 		},
 	})
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
 	}
 
 	return nil
---
+++ cmd/httpGet.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+// for control over HTTP client headers,
+// redirect policy, and other settings,
+// A Client is an HTTP client
+func httpGet(client *http.Client, url string, params map[string]string) (string, error) {
+	// Create a new request using http
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	// add parameters to request
+	q := req.URL.Query()
+	for key, value := range params {
+		q.Add(key, value)
+	}
+	req.URL.RawQuery = q.Encode()
+
+	// Send req using http Client
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Call ReadAll to get the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+
+}
